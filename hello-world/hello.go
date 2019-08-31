fe511cb5c2408b21cd1f8223f837ab67232895fd
Darshan Crout <dtcrout@gmail.com>
2019-08-30 22:07:45 -0400

    Add subtasks to testing function and add more functionality to Hello()

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,12 +2,20 @@ package main
 
 import "fmt"
 
+// Defining constants can help improve the performance of the code
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
 	// Hello() returns a string
-	// types are added after name, e.g. name string
-	return "Hello, " + name
+	// Types are added after name, e.g. name string
+
+	if name == "" {
+		name = "World"
+	}
+
+	return englishHelloPrefix + name
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("Darshan"))
 }
--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -12,13 +12,31 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	// got := Hello()
-	got := Hello("Darshan")
-	// want := "Hello, world"
-	// want := "Hello world"
-	want := "Hello, Darshan"
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
+	// Assertion message for subtasks wrapped into a function
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		// t.Helper() is needed to tell the test suite the function is a helper
+		// If this is commented out, the line number reported back will be this
+		// if statement instead of where it actually fails
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
 	}
+
+	// Subtest for argument in Hello()
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Darshan")
+		want := "Hello, Darshan"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	// Subtest for empty argument in Hello()
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
 }
