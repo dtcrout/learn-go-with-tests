d46cff904f49f8c8f3402795ba3fb22372ebffb3
Darshan Crout <dtcrout@gmail.com>
2019-09-03 21:48:27 -0400

    Complete structs and interfaces example

--- README.md
+++ README.md
@@ -35,5 +35,6 @@ This is my own personal repository for the [Learn Go with Tests](https://quii.gi
 
 ## Notes
 
-* Golang docs are available locally and can be launched via `$ godoc -http :8000`
+* Golang docs are available locally and can be launched via `$ godoc -http :8000**
 * Having too many tests can become a problem in the long run; every test has its cost. Therefore the goal is not to have as many tests as possible, rather to have confidence in your codebase
+* Interfaces allow you to make functions that can be used with different types
--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -13,25 +13,28 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
+	// Example of table testing
+	// Create a slice of struct
+	// Add test cases
+	// If in the future we add a new shape, we can
+	// simply add the case here
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{shape: Rectangle{12, 6}, hasArea: 72.0},
+		{shape: Circle{10}, hasArea: 314.1592653589793},
+		{shape: Triangle{12, 6}, hasArea: 36.0},
+	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		got := rectangle.Area()
-		want := 72.0
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		got := circle.Area()
-		want := 314.1592653589793
-
-		if got != want {
-			t.Errorf("got %.2f want %.2f", got, want)
-		}
-	})
-
+	// Iterate over areaTests cases
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.shape.Area() {
+				t.Errorf("%#v got %.2f want %.2f", tt.shape, got, tt.hasArea)
+			}
+		})
+	}
 }
